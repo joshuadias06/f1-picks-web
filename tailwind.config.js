9a9c00d70c9ca58cd3caa55a13d2eadaefd08c70
joshuadias06 <joshuadiasbf@gmail.com>
2025-11-11 14:59:46 -0300

    First commit - create project and install dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E10600",
+        dark: "#0A0A0A",
+        metallic: "#2C2C2C",
+        ice: "#F5F5F5",
+        blue: "#00ADEF",
+      },
+      fontFamily: {
+        orbitron: ["Orbitron", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
