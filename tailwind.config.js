b1143691326c26da28e9c480a9c199a5df4bfd0c
joshuadias06 <joshuadiasbf@gmail.com>
2025-11-11 15:50:04 -0300

    Create project structure and configure, font family project

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
         blue: "#00ADEF",
       },
       fontFamily: {
+        f1: ["F1 Regular", "sans-serif"],
+        "f1-bold": ["F1 Bold", "sans-serif"],
+        "f1-bold-web": ["F1 Bold Web", "sans-serif"],
+        "f1-black": ["F1 Black", "sans-serif"],
+        "f1-italic": ["F1 Italic", "sans-serif"],
+        "f1-wide": ["F1 Wide", "sans-serif"],
         orbitron: ["Orbitron", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
       },
